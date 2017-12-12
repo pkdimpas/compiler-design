ea1bccdfd1aa6554d306fd632a1c62a005a1481a
cgelbolingo <christiangelbolingo@gmail.com>
2017-12-12 15:51:58 +0800

    Regex Validations

--- Strings/src/strings/Strings.java
+++ Strings/src/strings/Strings.java
@@ -5,6 +5,8 @@
  */
 package strings;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,27 +22,56 @@ public class Strings {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         try{
             File input;
             Scanner sc;
             String s;
             StringBle sb;
+            Pattern p;
+            Matcher m;
+            
             
             input = new File("source.ble");
             sc = new Scanner(input);
             
             while(sc.hasNextLine()){
-                s = sc.nextLine();
-                sb = new StringBle("Hello");
-                System.out.println(sb.find("he"));
-                System.out.println(sb.find("ge"));
-                System.out.println(sb.toLower());
-                System.out.println(sb.toUpper());
+                s = sc.nextLine();   
+                //Getting the string
+                String regex = "Let(?:\\s+)[a-zA-Z]*(?:\\s*)=(?:\\s*)(\"|\')([\\w.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]+(?:\\s*))+\\1";
+                String str = searchRegex(s, regex);
+                if(str != null){
+                    System.out.println(str);
+                    regex = "(\\\"|\\')([\\w.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]+(?:\\s*))+\\1";
+                    str = searchRegex(s, regex);
+                    System.out.println(str);
+                    if(str != null){
+                        regex = "([\\w.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]+(?:\\s*))+";
+                        //System.out.println(str);
+                        str = searchRegex(str, regex);
+                        //System.out.println(str);
+                        if(str != null){
+                            System.out.println(str);
+                        }
+                    }
+                }
             }
         }catch(FileNotFoundException e){
             System.out.println(e.getMessage());
         }
         
+    }
+    
+    public static String searchRegex(String str, String regex){
+        Pattern p;
+        Matcher m;
+        String ret = null;
+        p = Pattern.compile(regex);
+        m = p.matcher(str);
+        if(m.find()){
+            ret = m.group();
+        }
+        return ret;
     }
     
 }
